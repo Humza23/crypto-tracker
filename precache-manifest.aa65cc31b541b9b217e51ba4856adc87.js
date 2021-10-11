4a6be3801dfe90f070466e8c03a2bb4aa99aff32
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-10-11 18:59:13 -0400

    Updates

---
+++ precache-manifest.aa65cc31b541b9b217e51ba4856adc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a87bbe00e578a53ef470f720e2ecebf",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "1c713efa8dc6ac501f75",
+    "url": "/crypto-tracker/static/css/main.da8208b2.chunk.css"
+  },
+  {
+    "revision": "e6d94ff3000d12584028",
+    "url": "/crypto-tracker/static/js/2.40b376f0.chunk.js"
+  },
+  {
+    "revision": "e12ed09be89d98e3b07b38f179289140",
+    "url": "/crypto-tracker/static/js/2.40b376f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c713efa8dc6ac501f75",
+    "url": "/crypto-tracker/static/js/main.055886d9.chunk.js"
+  },
+  {
+    "revision": "9784d1b35004ec98498b",
+    "url": "/crypto-tracker/static/js/runtime-main.9faddbba.js"
+  }
+]);
