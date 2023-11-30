fb6e9c3c83e5036a9d5ba4efe474714062c48e56
Humza <humza718@gmail.com>
2023-11-30 14:25:09 -0500

    error message

---
+++ src/components/ErrorComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorComponent = ({ errorMessage }) => {
+  return (
+    <div className="error-container">
+      <p className="error-message">{errorMessage}</p>
+    </div>
+  );
+};
+
+export default ErrorComponent;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import ErrorComponent from "./components/ErrorComponent.js";
+import SplashPage from "./components/SplashPage.js";
 
 import useDarkMode from './hooks/useDarkMode.js'
 
@@ -12,19 +14,44 @@ import "./styles.scss";
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useDarkMode(false);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        "https://ai.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => {  
+        setCoinData(res.data)
+        setLoading(false)
+      }) 
+      .catch((err) => {
+        console.log(err)
+        setError('Error fetching data, please try again later')
+        setLoading(false);
+      });
+    }, []);
+
+  useEffect(() => {
+    const splashTimeout = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(splashTimeout);
   }, []);
+  
+
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      {loading ?
+      (<SplashPage />) :
+      error ?
+      (<ErrorComponent errorMessage={error} /> ) :
+(     <>
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Charts coinData={coinData} />
+      </>
+)
+      }
     </div>
   );
 };
